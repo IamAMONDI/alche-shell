ba904e66c1e9bd2f01da4e8cf1caffe6c31e7645
IamAMONDI <e.otieno1@alustudent.com>
2026-03-24 01:20:10 -0700

    main script file

--- permissions/scriptfile.sh
+++ permissions/scriptfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 touch 2-groups  3-new_owner  4-empty hello 
-echo -e '#!/bin/bash\groups' > 2-groups
-echo -e '#!/bin/bash\betty/hello' > 3-new_owner
+echo -e '#!/bin/bash\ngroups' > 2-groups
+echo -e '#!/bin/bash\nbetty/hello' > 3-new_owner
 chmod +x 2-groups  3-new_owner  4-empty 
 git add .
 git commit -m "main script file"
